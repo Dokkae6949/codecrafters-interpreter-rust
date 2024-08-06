b806fcd82fbc772a8ee89fc7f6c0b54b6a49df01
Dokkae6949 <finnliry@gmail.com>
2024-08-06 18:20:34 +0200

    Add number parsing for lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,87 +9,118 @@ impl Lexer {
         while let Some(char) = chars.next() {
             match char {
                 '\n' => line_number += 1,
-                '\t' => { },
-                ' ' => { },
-                '(' => println!("LEFT_PAREN {} null", char),
-                ')' => println!("RIGHT_PAREN {} null", char),
-                '{' => println!("LEFT_BRACE {} null", char),
-                '}' => println!("RIGHT_BRACE {} null", char),
-                '+' => println!("PLUS {} null", char),
-                '-' => println!("MINUS {} null", char),
-                '*' => println!("STAR {} null", char),
-                ',' => println!("COMMA {} null", char),
-                '.' => println!("DOT {} null", char),
-                ';' => println!("SEMICOLON {} null", char),
+                '\t' | ' ' => {},
+                '(' => println!("LEFT_PAREN ( null"),
+                ')' => println!("RIGHT_PAREN ) null"),
+                '{' => println!("LEFT_BRACE {{ null"),
+                '}' => println!("RIGHT_BRACE }} null"),
+                '+' => println!("PLUS + null"),
+                '-' => println!("MINUS - null"),
+                '*' => println!("STAR * null"),
+                ',' => println!("COMMA , null"),
+                '.' => println!("DOT . null"),
+                ';' => println!("SEMICOLON ; null"),
                 '=' => {
                     if chars.peek() == Some(&'=') {
-                        println!("EQUAL_EQUAL {}{} null", char, chars.next().unwrap());
+                        println!("EQUAL_EQUAL == null");
+                        chars.next();
                     } else {
-                        println!("EQUAL {} null", char);
+                        println!("EQUAL = null");
                     }
                 },
                 '!' => {
                     if chars.peek() == Some(&'=') {
-                        println!("BANG_EQUAL {}{} null", char, chars.next().unwrap());
+                        println!("BANG_EQUAL != null");
+                        chars.next();
                     } else {
-                        println!("BANG {} null", char);
+                        println!("BANG ! null");
                     }
                 },
                 '<' => {
                     if chars.peek() == Some(&'=') {
-                        println!("LESS_EQUAL {}{} null", char, chars.next().unwrap());
+                        println!("LESS_EQUAL <= null");
+                        chars.next();
                     } else {
-                        println!("LESS {} null", char);
+                        println!("LESS < null");
                     }
                 },
                 '>' => {
                     if chars.peek() == Some(&'=') {
-                        println!("GREATER_EQUAL {}{} null", char, chars.next().unwrap());
+                        println!("GREATER_EQUAL >= null");
+                        chars.next();
                     } else {
-                        println!("GREATER {} null", char);
+                        println!("GREATER > null");
                     }
                 },
                 '/' => {
                     if chars.peek() == Some(&'/') {
-                        while chars.peek() != Some(&'\n') && chars.peek() != None {
-                            chars.next();
-                        }
+                        while chars.next_if(|&c| c != '\n').is_some() {}
                     } else {
-                        println!("SLASH {} null", char);
+                        println!("SLASH / null");
                     }
                 },
                 '"' => {
-                    let mut terminated = false;
                     let mut string = String::new();
+                    let mut terminated = false;
 
                     while let Some(c) = chars.next() {
                         if c == '"' {
                             terminated = true;
                             break;
                         }
-
                         if c == '\n' {
                             line_number += 1;
                         }
-
                         string.push(c);
                     }
 
-                    if !terminated {
+                    if terminated {
+                        println!("STRING \"{}\" {}", string, string);
+                    } else {
                         status = 65;
                         eprintln!("[line {}] Error: Unterminated string.", line_number);
+                    }
+                },
+                '0'..='9' => {
+                    let mut number = char.to_string();
+                    let mut has_dot = false;
+
+                    while let Some(&c) = chars.peek() {
+                        if c.is_numeric() {
+                            number.push(c);
+                            chars.next();
+                        } else if c == '.' && !has_dot {
+                            let mut lookahead = chars.clone();
+                            lookahead.next(); // consume the dot
+                            if let Some(next_char) = lookahead.next() {
+                                if !next_char.is_numeric() { break; }
+                                has_dot = true;
+                                number.push(c);
+                                chars.next();
+                            } else {
+                                break;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+
+                    let parsed = number.parse::<f64>().unwrap();
+
+                    if parsed.fract() == 0.0 {
+                        println!("NUMBER {} {:.1}", number, parsed);
                     } else {
-                        println!("STRING \"{}\" {}", string, string);
+                        println!("NUMBER {} {}", number, parsed);
                     }
                 },
                 _ => {
                     status = 65;
                     eprintln!("[line {}] Error: Unexpected character: {}", line_number, char);
-                },
-            };
+                }
+            }
         }
 
-        println!("EOF  null");
+        println!("EOF null");
         status
     }
 }
