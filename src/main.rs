b7851dbc3b36d23035f09aeeb75b2dbf6c6612fc
Dokkae6949 <finnliry@gmail.com>
2024-08-06 20:44:05 +0200

    Add back legexer printing

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,64 @@ fn main() {
                 String::new()
             });
 
-            match Lexer::tokenize(&file_contents) {
-                Ok(_) => exit(0),
+            let tokens = match Lexer::tokenize(&file_contents) {
+                Ok(tokens) => tokens,
                 Err(status) => exit(status)
             };
+
+            for token in tokens.iter() {
+                match &token.kind {
+                    TokenKind::Eof => println!("EOF  null"),
+                    TokenKind::LeftParen => println!("LEFT_PAREN ( null"),
+                    TokenKind::RightParen => println!("RIGHT_PAREN ) null"),
+                    TokenKind::LeftBrace => println!("LEFT_BRACE {{ null"),
+                    TokenKind::RightBrace => println!("RIGHT_BRACE }} null"),
+                    TokenKind::Plus => println!("PLUS + null"),
+                    TokenKind::Minus => println!("MINUS - null"),
+                    TokenKind::Star => println!("STAR * null"),
+                    TokenKind::Comma => println!("COMMA , null"),
+                    TokenKind::Dot => println!("DOT . null"),
+                    TokenKind::Semicolon => println!("SEMICOLON ; null"),
+                    TokenKind::Equal => println!("EQUAL = null"),
+                    TokenKind::EqualEqual => println!("EQUAL_EQUAL == null"),
+                    TokenKind::Bang => println!("BANG ! null"),
+                    TokenKind::BangEqual => println!("BANG_EQUAL != null"),
+                    TokenKind::Less => println!("LESS < null"),
+                    TokenKind::LessEqual => println!("LESS_EQUAL <= null"),
+                    TokenKind::Greater => println!("GREATER > null"),
+                    TokenKind::GreaterEqual => println!("GREATER_EQUAL >= null"),
+                    TokenKind::Slash => println!("SLASH / null"),
+                    TokenKind::String(s) => println!("STRING \"{}\" {}", s, s),
+                    TokenKind::Number(n) => {
+                        if n.fract() == 0.0 {
+                            println!("NUMBER {} {:.1}", n, n)
+                        } else {
+                            println!("NUMBER {} {}", n, n)
+                        }
+                    }
+                    TokenKind::Identifier(s) => println!("IDENTIFIER {} null", s),
+                    TokenKind::Keyword(keyword) => {
+                        match keyword {
+                            Keyword::Nil => println!("NIL nil null"),
+                            Keyword::And => println!("AND and null"),
+                            Keyword::Class => println!("CLASS class null"),
+                            Keyword::Else => println!("ELSE else null"),
+                            Keyword::False => println!("FALSE false null"),
+                            Keyword::For => println!("FOR for null"),
+                            Keyword::Fun => println!("FUN fun null"),
+                            Keyword::If => println!("IF if null"),
+                            Keyword::Or => println!("OR or null"),
+                            Keyword::Print => println!("PRINT print null"),
+                            Keyword::Return => println!("RETURN return null"),
+                            Keyword::Super => println!("SUPER super null"),
+                            Keyword::This => println!("THIS this null"),
+                            Keyword::True => println!("TRUE true null"),
+                            Keyword::Var => println!("VAR var null"),
+                            Keyword::While => println!("WHILE while null"),
+                        }
+                    }
+                }
+            }
         }
         "evaluate" => {
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
